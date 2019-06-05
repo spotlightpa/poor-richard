4ae68fe3fe971f0d288902f807dd584dbe5a0dc5
Carl Johnson <me@carlmjohnson.net>
2019-06-05 09:11:26 -0400

    JS: Handle null queries

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,12 @@ function each(qs, cb) {
   if (typeof qs === "string") {
     qs = document.querySelectorAll(qs);
   }
+  if (!qs) {
+    return;
+  }
+  if (qs.length === undefined) {
+    qs = [qs];
+  }
   for (var i = 0; i < qs.length; i++) {
     cb(qs[i], i);
   }
@@ -13,15 +19,14 @@ function on(ev, qs, cb) {
   });
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+on("DOMContentLoaded", document, function() {
   on("click", "[data-target]", function(ev) {
     var targets = ev.currentTarget.getAttribute("data-target");
     var toggleClass = ev.currentTarget.getAttribute("data-toggle-class");
     toggleClass = toggleClass ? toggleClass : "is-active";
-    if (targets) {
-      each(targets, function(el) {
-        el.classList.toggle(toggleClass);
-      });
-    }
+
+    each(targets, function(el) {
+      el.classList.toggle(toggleClass);
+    });
   });
 });
