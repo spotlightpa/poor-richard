315a7f40fb5f9c395a864ff5b48dbb3ac93c5543
Carl Johnson <me@carlmjohnson.net>
2019-09-16 14:28:26 -0400

    Create a tracker redirect file

---
+++ assets/js/main-tracker.js
@@ -0,0 +1,38 @@
+(() => {
+  let dnt = navigator.doNotTrack || window.doNotTrack || navigator.msDoNotTrack;
+  let doNotTrack = dnt == "1" || dnt == "yes";
+  if (doNotTrack) {
+    return;
+  }
+
+  window.ga =
+    window.ga ||
+    function() {
+      (ga.q = ga.q || []).push(arguments);
+    };
+
+  ga.l = +new Date();
+
+  ga("create", "UA-141126362-3", "auto", "spotlightpa");
+  ga("spotlightpa.set", "anonymizeIp", true);
+  ga("spotlightpa.send", "pageview");
+
+  function ensureGA() {
+    let hasGA = Array.from(document.scripts).find(el =>
+      el.src.match(/google-analytics/)
+    );
+    if (hasGA) {
+      return;
+    }
+    let s = document.createElement("script");
+    s.async = true;
+    s.src = "https://www.google-analytics.com/analytics.js";
+    document.body.append(s);
+  }
+
+  if (document.readyState === "loading") {
+    window.addEventListener("load", ensureGA);
+  } else {
+    ensureGA();
+  }
+})();
