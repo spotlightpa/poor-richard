c4eca3bd81133eb0443761043a9af5fab42aaa25
Carl Johnson <me@carlmjohnson.net>
2019-09-16 14:27:48 -0400

    JS: Pull out dom-utils package

---
+++ assets/js/dom-utils.js
@@ -0,0 +1,20 @@
+export function each(qs, cb) {
+  if (typeof qs === "string") {
+    qs = document.querySelectorAll(qs);
+  }
+  if (!qs) {
+    return;
+  }
+  if (qs.length === undefined) {
+    qs = [qs];
+  }
+  for (var i = 0; i < qs.length; i++) {
+    cb(qs[i], i);
+  }
+}
+
+export function on(ev, qs, cb) {
+  each(qs, el => {
+    el.addEventListener(ev, cb);
+  });
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,18 @@
-function each(qs, cb) {
-  if (typeof qs === "string") {
-    qs = document.querySelectorAll(qs);
-  }
-  if (!qs) {
-    return;
-  }
-  if (qs.length === undefined) {
-    qs = [qs];
-  }
-  for (var i = 0; i < qs.length; i++) {
-    cb(qs[i], i);
-  }
-}
-
-function on(ev, qs, cb) {
-  each(qs, (el) => {
-    el.addEventListener(ev, cb);
-  });
-}
+import { each, on } from "./dom-utils.js";
 
 function createListeners() {
-  on("click", "[data-target]", (ev)=> {
+  on("click", "[data-target]", ev => {
     var targets = ev.currentTarget.dataset.target;
     var toggleClass = ev.currentTarget.dataset.toggleClass;
     toggleClass = toggleClass ? toggleClass : "is-active";
 
-    each(targets, (el) => {
+    each(targets, el => {
       el.classList.toggle(toggleClass);
     });
   });
 
   // Open external links in new window
-  each("a[href^=http]", (el) => {
+  each("a[href^=http]", el => {
     if (el.host === window.location.host) {
       return;
     }
@@ -39,7 +20,7 @@ function createListeners() {
     el.rel = "noopener noreferrer";
   });
 
-  on("click", '[data-share="tweet"]', (ev)=> {
+  on("click", '[data-share="tweet"]', ev => {
     var tweet = ev.currentTarget.dataset.shareText;
     var url = ev.currentTarget.dataset.shareUrl;
     url = url ? url : window.location.href;
@@ -53,7 +34,7 @@ function createListeners() {
     window.open(twitterURL, "_blank");
   });
 
-  on("click", '[data-share="facebook"]', (ev)=> {
+  on("click", '[data-share="facebook"]', ev => {
     var url = ev.currentTarget.dataset.shareUrl;
     url = url ? url : window.location.href;
     var facebookURL =
