28d84b1b883ddd210805f98945660fa866bf7b91
Carl Johnson <me@carlmjohnson.net>
2019-09-26 14:05:01 -0400

    More complicated showing it logic

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,6 +1,6 @@
 import { on, loadDate, storeDate } from "./dom-utils.js";
 
-function openModal() {
+export function openModal() {
   let modal = document.querySelector("#modal-newsletter");
   if (!modal) {
     return;
@@ -38,17 +38,28 @@ function openModal() {
 export function addModal() {
   let cancelOpen = on("scroll", [window], () => {
     cancelOpen();
+    const LAST_VISIT_KEY = "last-visit";
     const SAW_NEWSLETTER_MODAL_KEY = "saw-newsletter-modal";
     const FROM_MC_KEY = "originated-from-mailchimp";
-    let now = new Date();
-    let showModal = true;
+    const delay = 2000; // 2s
 
+    let now = new Date();
+    let showModal = false;
+
+    // If this is our second+ page seen...
+    if (loadDate(LAST_VISIT_KEY)) {
+      showModal = true;
+    }
+    storeDate(LAST_VISIT_KEY, now);
+
+    // And we're not already on the newsletter page...
     if (window.location.pathname.match(/newsletters/)) {
       storeDate(SAW_NEWSLETTER_MODAL_KEY, now);
     }
     if (loadDate(SAW_NEWSLETTER_MODAL_KEY)) {
       showModal = false;
     }
+    // And didn't come from the newsletter...
     if (window.location.href.match(/utm_source=email/)) {
       storeDate(FROM_MC_KEY, now);
     }
@@ -58,12 +69,14 @@ export function addModal() {
     if (loadDate(FROM_MC_KEY)) {
       showModal = false;
     }
+    // Or we're just testing...
     if (window.location.href.match(/debug-modal/)) {
       showModal = true;
     }
+    // Then show it...
     if (showModal) {
       storeDate(SAW_NEWSLETTER_MODAL_KEY, now);
-      window.setTimeout(openModal, 500);
+      window.setTimeout(openModal, delay);
     }
   });
 }
