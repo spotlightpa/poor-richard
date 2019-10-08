f6cdfa1a3e12cdc84e3323480de78583f17f2e88
Carl Johnson <me@carlmjohnson.net>
2019-10-08 13:16:05 -0400

    Donation form: Add Google Analytics events (#38)

--- assets/js/main-donate.js
+++ assets/js/main-donate.js
@@ -2,6 +2,23 @@ import Vue from "vue";
 import DonationForm from "./components/DonationForm.vue";
 import { each } from "./dom-utils.js";
 
+if (!window.ga) {
+  window.ga = function() {
+    (window.ga.q = window.ga.q || []).push(arguments);
+  };
+  window.ga.l = +new Date();
+}
+
+let GA = {
+  install(Vue) {
+    Vue.prototype.$ga = function() {
+      window.ga(arguments);
+    };
+  }
+};
+
+Vue.use(GA);
+
 each("[data-vue=donation-form]", el => {
   new Vue(DonationForm).$mount(el);
 });
