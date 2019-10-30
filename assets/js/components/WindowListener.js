4e20964476b6d205bc21eeb06ae4951a7151c7c3
Carl Johnson <me@carlmjohnson.net>
2019-10-30 14:30:34 -0400

    Donation: Refactor window listener

---
+++ assets/js/components/WindowListener.js
@@ -0,0 +1,22 @@
+export default {
+  name: "WindowListener",
+  props: {
+    event: String
+  },
+  mounted() {
+    let eventName = this.event;
+    let listener = ev => {
+      this.$emit(eventName, ev);
+    };
+
+    window.addEventListener(eventName, listener);
+    this.removeListener = () =>
+      void window.removeEventListener(eventName, listener);
+  },
+  beforeDestroy() {
+    this.removeListener && this.removeListener();
+  },
+  render() {
+    return;
+  }
+};
