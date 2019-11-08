e2d4e166fa7fba84fca0962986e9e9d17c07befb
Carl Johnson <me@carlmjohnson.net>
2019-11-08 10:27:04 -0500

    PostCSS: Make compatible with Parcel

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,14 @@
 const purgecss = require("@fullhuman/postcss-purgecss");
 
-// eslint-disable-next-line no-unused-vars
-module.exports = ctx => {
-  let plugins = [require("autoprefixer")];
+let plugins = [require("autoprefixer")];
+if (process.env.NODE_ENV === "production") {
+  plugins.push(
+    purgecss({
+      content: ["./**/*.html", "./**/*.vue"]
+    })
+  );
+}
 
-  if (process.env.NODE_ENV === "production") {
-    plugins.push(
-      purgecss({
-        content: ["./**/*.html", "./**/*.vue"]
-      })
-    );
-  }
-  return { plugins };
+module.exports = {
+  plugins
 };
