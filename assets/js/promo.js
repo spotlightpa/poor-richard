017cb3d9b403468ea3200f090e6927c706a7f403
Carl Johnson <me@carlmjohnson.net>
2019-11-15 11:59:50 -0500

    Add donate inline promo

---
+++ assets/js/promo.js
@@ -0,0 +1,14 @@
+import { each } from "./dom-utils.js";
+
+function randomChoice(a) {
+  let i = Math.floor(Math.random() * a.length);
+  return a[i];
+}
+
+export function rotatePromoImages() {
+  each("[data-promo-images]", el => {
+    let images = JSON.parse(el.dataset.promoImages);
+    let image = randomChoice(images);
+    el.setAttribute("xlink:href", image);
+  });
+}
