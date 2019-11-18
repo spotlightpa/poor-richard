3b0ae9befe3af7719b84bf3530475e07183de90e
Carl Johnson <me@carlmjohnson.net>
2019-11-18 11:44:29 -0500

    JS: Rework Google Events

--- assets/js/google-analytics.js
+++ assets/js/google-analytics.js
@@ -1,3 +1,5 @@
+import { each, on, polyfillClosest } from "./dom-utils.js";
+
 // Ensure a Google Analytics window func
 if (!window.ga) {
   window.ga = function() {
@@ -25,4 +27,52 @@ export function ensureGA() {
   s.async = true;
   s.src = "https://www.google-analytics.com/analytics.js";
   document.body.appendChild(s);
+}
+
+export function addGAListeners() {
+  polyfillClosest();
+
+  each("a", el => {
+    let isInternal =
+      el.host === window.location.host || el.host.match(/spotlightpa\.org$/);
+
+    // Open external links in new window
+    if (!isInternal) {
+      el.target = "_blank";
+      el.rel = "noopener noreferrer";
+    }
+
+    on("click", el, ev => {
+      let { gaEvent } = ev.currentTarget.dataset;
+      if (gaEvent) {
+        gaEvent = JSON.parse(gaEvent);
+        sendGAEvent(gaEvent);
+        return;
+      }
+
+      let eventCategory = isInternal ? "Internal Link" : "Outbound Link";
+      let eventAction = "click";
+      let parentAction = el.closest("[data-ga-action]");
+      if (parentAction) {
+        eventAction = parentAction.dataset.gaAction;
+      }
+
+      sendGAEvent({
+        eventCategory,
+        eventAction,
+        eventLabel: ev.currentTarget.href,
+        transport: "beacon"
+      });
+    });
+  });
+
+  on("submit", "[data-ga-form]", ev => {
+    let form = ev.target;
+    let isValid = form.reportValidity();
+
+    let gaEvent = JSON.parse(ev.currentTarget.dataset.gaForm);
+    gaEvent.transport = "beacon";
+    gaEvent.eventLabel = isValid ? "submit" : "invalid submission";
+    sendGAEvent(gaEvent);
+  });
 }
