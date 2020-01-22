1ec5796da6eaa6d64f323372152c053ec324816d
Carl Johnson <me@carlmjohnson.net>
2020-01-22 15:37:45 -0500

    Social buttons: Add share sheet

--- src/utils/social.js
+++ src/utils/social.js
@@ -1,13 +1,13 @@
-import { on } from "./dom-utils.js";
+import { each, on } from "./dom-utils.js";
 import { sendGAEvent } from "./google-analytics.js";
 
 export function addSocialButtonListeners() {
   on("click", '[data-share="tweet"]', ev => {
-    var tweet = ev.currentTarget.dataset.shareText;
-    var url = ev.currentTarget.dataset.shareUrl;
+    let tweet = ev.currentTarget.dataset.shareText;
+    let url = ev.currentTarget.dataset.shareUrl;
     url = url ? url : window.location.href;
 
-    var twitterURL =
+    let twitterURL =
       "https://twitter.com/intent/tweet?text=" +
       encodeURIComponent(tweet) +
       "&url=" +
@@ -22,9 +22,9 @@ export function addSocialButtonListeners() {
   });
 
   on("click", '[data-share="facebook"]', ev => {
-    var url = ev.currentTarget.dataset.shareUrl;
+    let url = ev.currentTarget.dataset.shareUrl;
     url = url ? url : window.location.href;
-    var facebookURL =
+    let facebookURL =
       "https://www.facebook.com/dialog/feed?app_id=589296315209793&display=page&link=" +
       encodeURIComponent(url);
     window.open(facebookURL, "_blank");
@@ -34,4 +34,28 @@ export function addSocialButtonListeners() {
       eventLabel: url
     });
   });
+
+  on("click", '[data-share="sharesheet"]', ev => {
+    let title = ev.currentTarget.dataset.shareTitle ?? document.title;
+    let text = ev.currentTarget.dataset.shareText ?? "";
+    let url = ev.currentTarget.dataset.shareUrl ?? window.location.href;
+    navigator
+      .share({
+        title,
+        text,
+        url
+      })
+      // Ignore errors caused by closing sheet
+      .catch(() => null);
+
+    sendGAEvent({
+      eventCategory: "Share button interaction",
+      eventAction: "Open share sheet",
+      eventLabel: url
+    });
+  });
+
+  if ("share" in navigator) {
+    each('[data-share="sharesheet"]', el => el.classList.remove("is-hidden"));
+  }
 }
