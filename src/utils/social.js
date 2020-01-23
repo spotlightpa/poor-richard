964e2959a9aad5819199b7e97f3a91e02a089194
Carl Johnson <me@carlmjohnson.net>
2020-01-23 16:12:03 -0500

    Social buttons: Better share sheet text, only do GA on success

--- src/utils/social.js
+++ src/utils/social.js
@@ -36,8 +36,14 @@ export function addSocialButtonListeners() {
   });
 
   on("click", '[data-share="sharesheet"]', ev => {
-    let title = ev.currentTarget.dataset.shareTitle ?? document.title;
-    let text = ev.currentTarget.dataset.shareText ?? "";
+    let title =
+      ev.currentTarget.dataset.shareTitle ??
+      document.querySelector("[itemprop='name']")?.content ??
+      document.title;
+    let text =
+      ev.currentTarget.dataset.shareText ??
+      document.querySelector("[itemprop='description']")?.content ??
+      "";
     let url = ev.currentTarget.dataset.shareUrl ?? window.location.href;
     navigator
       .share({
@@ -45,14 +51,15 @@ export function addSocialButtonListeners() {
         text,
         url
       })
+      .then(() =>
+        sendGAEvent({
+          eventCategory: "Share button interaction",
+          eventAction: "Open share sheet",
+          eventLabel: url
+        })
+      )
       // Ignore errors caused by closing sheet
       .catch(() => null);
-
-    sendGAEvent({
-      eventCategory: "Share button interaction",
-      eventAction: "Open share sheet",
-      eventLabel: url
-    });
   });
 
   if ("share" in navigator) {
