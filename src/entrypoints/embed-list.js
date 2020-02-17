d07489db350738136e2c5ad7cd086d61b4aa994b
Carl Johnson <me@carlmjohnson.net>
2020-02-17 14:29:30 -0500

    JS: Experimental use of Alpine.JS

--- src/entrypoints/embed-list.js
+++ src/entrypoints/embed-list.js
@@ -1,21 +1,63 @@
-import Vue from "vue";
+import "alpinejs";
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCopy } from "@fortawesome/free-regular-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+window.splEmbedList = () => ({
+  embedCode: "",
+  baseURL: "",
+  paramEls: null,
+  srcEl: null,
+  codeEl: null,
+  debounceID: null,
+  showCopied: false,
 
-library.add(faCopy);
+  init($el, $refs) {
+    this.baseURL = $el.dataset.url;
+    this.paramEls = Array.from($el.querySelectorAll("input[name]"));
+    this.srcEl = $el.querySelector("[data-spl-src]");
+    this.codeEl = $refs.code;
+    this.embedCodeEl = $refs.embedCodeEl;
+    this.setEmbedCode();
+  },
+  setEmbedCode() {
+    // go through the params and make the URL
+    let url = this.baseURL;
+    let params = [];
+    for (let paramEl of this.paramEls) {
+      let { name, value } = paramEl;
+      if (value) {
+        params.push({ name, value });
+      }
+    }
+    if (params.length) {
+      url +=
+        "?" +
+        params
+          .map(({ name, value }) => name + "=" + encodeURIComponent(value))
+          .join("&");
+    }
+    // set URL on the obj
+    this.srcEl.dataset.splSrc = url;
+    if (this.srcEl.shadowRoot) {
+      this.srcEl.shadowRoot.querySelector("iframe").src = url;
+    }
+    // get embedCode from the inner HTML
+    this.embedCode = this.codeEl.innerHTML.trim();
+  },
+  copy() {
+    let selection = window.getSelection();
+    selection.removeAllRanges();
+    let range = document.createRange();
+    range.selectNodeContents(this.embedCodeEl);
+    selection.addRange(range);
 
-Vue.component("font-awesome-icon", FontAwesomeIcon);
-
-import EmbedListWrapper from "../components/EmbedListWrapper.vue";
-import { each } from "../utils/dom-utils.js";
-
-var EmbedListWrapperComp = Vue.extend(EmbedListWrapper);
-
-each(".js-embed-list-item", el => {
-  let data = JSON.parse(el.innerText);
-  new EmbedListWrapperComp({
-    propsData: { data }
-  }).$mount(el);
+    if (document.execCommand("copy")) {
+      setTimeout(() => {
+        this.showCopied = true;
+        setTimeout(() => (this.showCopied = false), 5000);
+      }, 500);
+    }
+  },
+  updateParam() {
+    clearTimeout(this.debounceID);
+    this.debounceID = setTimeout(() => this.setEmbedCode(), 500);
+  }
 });
