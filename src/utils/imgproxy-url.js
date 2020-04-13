a8b522f1a5267d15b8916d8336ef07773de7363e
Carl Johnson <me@carlmjohnson.net>
2020-04-13 10:19:23 -0400

    HP: Add read more load in place

---
+++ src/utils/imgproxy-url.js
@@ -0,0 +1,17 @@
+export default function imageURL(
+  filepath,
+  { width = 400, height = 300, extension = "jpeg" } = {}
+) {
+  if (!filepath) {
+    return "";
+  }
+  let baseURL = "https://images.data.spotlightpa.org";
+  let signature = "insecure";
+  let resizing_type = "fill";
+  let gravity = "sm";
+  let enlarge = "1";
+  let quality = "75";
+  let encoded_source_url = btoa(filepath);
+
+  return `${baseURL}/${signature}/rs:${resizing_type}:${width}:${height}/g:${gravity}/el:${enlarge}/q:${quality}/${encoded_source_url}.${extension}`;
+}
---
+++ src/utils/read-more.js
@@ -0,0 +1,67 @@
+import "alpinejs";
+import imgproxy from "./imgproxy-url.js";
+
+window.spl = window.spl || {};
+
+window.spl.readmore = () => {
+  return {
+    hasLoaded: false,
+    isLoading: false,
+    hasClicked: false,
+    counter: 0,
+    fetchedStories: [],
+    error: null,
+
+    load() {
+      if (this.isLoading || this.hasLoaded) return;
+      this.isLoading = true;
+
+      let hrefs = new Set(
+        Array.from(this.$el.querySelectorAll("a")).map((el) => el.href)
+      );
+      // Figure out the right image size to request
+      let { width, height } = this.$refs.archives.querySelector("figure img");
+      width = window.devicePixelRatio * width;
+      height = window.devicePixelRatio * height;
+
+      fetch("/news/feed-summary.json")
+        .then((rsp) => rsp.json())
+        .then((json) => {
+          this.isLoading = false;
+          this.hasLoaded = true;
+          this.error = null;
+          // remove links in this.$el
+          this.fetchedStories = json.items
+            .filter((item) => !hrefs.has(item.url))
+            .map((item) => toStory(item, { width, height }));
+        })
+        .catch((err) => {
+          this.isLoading = false;
+          this.error = err;
+        });
+    },
+
+    get stories() {
+      return this.fetchedStories.slice(0, this.counter);
+    },
+
+    get loadingClass() {
+      return this.hasClicked && this.isLoading ? "is-loading" : "";
+    },
+
+    click() {
+      this.load();
+      this.hasClicked = true;
+      this.counter += 10;
+    },
+  };
+};
+
+function toStory(data, { width, height }) {
+  return {
+    ...data,
+    imageURL: imgproxy(data.image, { width, height }),
+  };
+}
+
+export function load() {}
