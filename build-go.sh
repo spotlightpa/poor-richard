7e0cb0250e17ba4e60a443db8025333006d33174
Carl Johnson <me@carlmjohnson.net>
2020-04-23 16:12:29 -0400

    Build Go deps with version

---
+++ build-go.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+# Get the directory that this script file is in
+THIS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+cd "$THIS_DIR"
+
+go version
+
+GOPKG="github.com/spotlightpa/poor-richard/pkg/api"
+BUILD_VERSION="$(git rev-parse --short HEAD)"
+LDFLAGS="-X '$GOPKG.BuildVersion=$BUILD_VERSION'"
+
+GOBIN=$THIS_DIR/functions go install -ldflags "$LDFLAGS" ./cmd/...
