44739357a9fb72b054f51728cf252a8cea68952a
Carl Johnson <me@carlmjohnson.net>
2020-04-27 14:53:12 -0400

    Hmm

--- build-parcel.sh
+++ build-parcel.sh
@@ -11,13 +11,14 @@ cd "$THIS_DIR"
 if [[ -n "${NETLIFY_BUILD_BASE:-}" ]]; then
   PARCEL_CACHE_DIR=$NETLIFY_BUILD_BASE/cache/parcel
   mkdir -p "$PARCEL_CACHE_DIR"
+  ls "$PARCEL_CACHE_DIR" | wc
 else
   PARCEL_CACHE_DIR=$(pwd)/.cache
 fi
 
-PATH="./node_modules/.bin:$PATH"
+PATH="$THIS_DIR/node_modules/.bin:$PATH"
 
 parcel build src/entrypoints/* \
     --experimental-scope-hoisting \
     --public-url /@src \
-    --cache-dir $PARCEL_CACHE_DIR
+    --cache-dir "$PARCEL_CACHE_DIR"
