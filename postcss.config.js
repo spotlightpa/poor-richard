a5125e18c74c5dcdf642bcad9a5c5caf2629645b
Carl Johnson <me@carlmjohnson.net>
2020-04-27 11:07:59 -0400

    Config: PurgeCSS with new Hugo PostProcess

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,14 @@
-const purgecss = require("@fullhuman/postcss-purgecss");
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: [ './hugo_stats.json' ],
+    defaultExtractor: (content) => {
+        let els = JSON.parse(content).htmlElements;
+        return els.tags.concat(els.classes, els.ids);
+    }
+});
 
 let plugins = [require("autoprefixer")];
 if (process.env.NODE_ENV === "production") {
-  plugins.push(
-    purgecss({
-      content: ["./layouts/**/*.html", "./src/**/*.vue"],
-    })
-  );
+  plugins.push(purgecss);
 }
 
 module.exports = {
