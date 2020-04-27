79a16349620cca208ea01f7e7362701b0de487d5
Carl Johnson <me@carlmjohnson.net>
2020-04-27 11:46:57 -0400

    PurgeCSS: Try to fix fade

---
+++ src/components/PurgeCSSOverride.vue
@@ -0,0 +1,6 @@
+<!-- Make sure PurgeCSS does not purge these classes: -->
+<template>
+  <div
+    class="has-text-warning is-clipped is-pulled-left is-pulled-right is-16by9 is-3by2 fade-enter-active fade-enter fade-leave-active fade-leave-to"
+  ></div>
+</template>
