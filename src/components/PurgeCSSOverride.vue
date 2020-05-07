84ac22824001cb1f86ef4ade160d1c1d54fef129
Carl Johnson <me@carlmjohnson.net>
2020-05-07 14:35:00 -0400

    PurgeCSS: Include fade, remove others

--- src/components/PurgeCSSOverride.vue
+++ src/components/PurgeCSSOverride.vue
@@ -1,6 +1,6 @@
 <!-- Make sure PurgeCSS does not purge these classes: -->
 <template>
   <div
-    class="has-text-warning is-clipped is-pulled-left is-pulled-right is-16by9 is-3by2 fade-enter-active fade-enter fade-leave-active fade-leave-to slide-left-enter-active slide-left-leave-active slide-right-enter-active slide-right-leave-active slide-left-enter slide-right-leave-active slide-left-leave-active slide-right-enter"
+    class="has-text-warning is-clipped is-pulled-left is-pulled-right is-16by9 is-3by2"
   ></div>
 </template>
