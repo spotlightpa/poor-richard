77759b70b819267f43ad79a083856d745be982c7
Carl Johnson <me@carlmjohnson.net>
2020-05-28 15:23:12 -0400

    PurgeCSS: Fix tables?

--- src/components/PurgeCSSOverride.vue
+++ src/components/PurgeCSSOverride.vue
@@ -1,6 +1,6 @@
 <!-- Make sure PurgeCSS does not purge these classes: -->
 <template>
   <div
-    class="has-text-warning is-clipped is-pulled-left is-pulled-right is-16by9 is-3by2"
+    class="has-text-warning is-clipped is-pulled-left is-pulled-right is-16by9 is-3by2 is-bordered is-striped is-hoverable is-fullwidth"
   ></div>
 </template>
