f48e0ac66c3db35ae0576fdc0c3e8f7045178142
Carl Johnson <me@carlmjohnson.net>
2020-06-18 08:48:54 -0400

    JS: Add alert for gone links (#110)

--- src/entrypoints/main.js
+++ src/entrypoints/main.js
@@ -5,13 +5,24 @@ import "../utils/metrics.js";
 
 function createListeners() {
   on("click", "[data-target]", (ev) => {
-    var targets = ev.currentTarget.dataset.target;
-    var toggleClass = ev.currentTarget.dataset.toggleClass;
+    let targets = ev.currentTarget.dataset.target;
+    let toggleClass = ev.currentTarget.dataset.toggleClass;
     toggleClass = toggleClass ? toggleClass : "is-active";
 
     each(targets, (el) => {
       el.classList.toggle(toggleClass);
     });
+  });
+  on("click", "a[data-gone]", (ev) => {
+    ev.preventDefault();
+    let url = ev.currentTarget.dataset.gone;
+    let msg =
+      "The target of this link has been removed. " +
+      `It pointed to ${url}. ` +
+      "Would you like to try to visit anyway?";
+    if (window.confirm(msg)) {
+      window.location = url;
+    }
   });
 
   addGAListeners();
