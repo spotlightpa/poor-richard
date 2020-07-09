0bf7b964510543a1ce1e30ca3ff73f9e1b64400e
Carl Johnson <me@carlmjohnson.net>
2020-07-09 16:06:12 -0400

    API: Add healthcheck endpoint

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -137,13 +137,13 @@ const mailchimpFeed = "https://us3.campaign-archive.com/feed?u=4f6d92afd9b3de3dd
 
 func (app *appEnv) routes() http.Handler {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/api/healthcheck/", app.pingErr)
 	mux.HandleFunc("/api/most-popular", app.getMostPopular)
 	mux.Handle(
 		"/api/newsletter.json",
 		feed2json.Handler(
 			feed2json.StaticURLInjector(mailchimpFeed),
 			nil, nil, app.Logger.Printf, app.cacheControlMiddleware))
-
 	return app.versionMiddleware(mux)
 }
 
