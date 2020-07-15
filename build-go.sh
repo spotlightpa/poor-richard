23b11061fcac513dafcc85fa03dcf82194b94d02
Carl Johnson <me@carlmjohnson.net>
2020-07-15 10:49:40 -0400

    Build script: Output steps

--- build-go.sh
+++ build-go.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eu -o pipefail
+set -euo pipefail
 
 # Get the directory that this script file is in
 THIS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
@@ -8,7 +8,7 @@ THIS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 cd "$THIS_DIR"
 
 go version
-
+set -x
 GOPKG="github.com/spotlightpa/poor-richard/pkg/api"
 BUILD_VERSION="$(git rev-parse --short HEAD)"
 LDFLAGS="-X '$GOPKG.BuildVersion=$BUILD_VERSION'"
