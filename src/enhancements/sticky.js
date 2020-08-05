226ab582389c7b30760a1d88aab71ed6e3c191db
Carl Johnson <me@carlmjohnson.net>
2020-08-05 16:11:37 -0400

    Layout: Add shell for featured template (#127)
    Copying CSS/layout from https://github.com/spotlightpa/featured-template-exp. Thanks @kwhc!

--- src/enhancements/sticky.js
+++ src/enhancements/sticky.js
@@ -2,31 +2,14 @@ import {
   sendGAEvent,
   reportClick as analytics,
 } from "../utils/google-analytics.js";
-import randomChoice from "../utils/random-choice.js";
 
 export default function sticky() {
   return {
     analytics,
     isOpen: false,
-    images: [],
-    _imageURL: "",
     oldFocus: null,
 
-    init() {
-      this.images = JSON.parse(this.$el.dataset.images);
-      let img = new Image();
-      img.src = this.imageURL;
-    },
-
-    get imageURL() {
-      if (!this.images.length) {
-        return "";
-      }
-      if (!this._imageURL) {
-        this._imageURL = randomChoice(this.images);
-      }
-      return this._imageURL;
-    },
+    init() {},
 
     get stickyClass() {
       return this.isOpen ? "is-active" : "";
@@ -35,8 +18,9 @@ export default function sticky() {
     show() {
       this.isOpen = true;
       sendGAEvent({
-        eventCategory: "Sticky Banner",
-        eventAction: "Show sticky banner",
+        eventCategory: "news:page:featured",
+        eventAction: "modal:sticky",
+        eventLabel: "sticky:open",
       });
       this.oldFocus = document.activeElement;
       const transitionLength = 500;
@@ -47,8 +31,9 @@ export default function sticky() {
       this.isOpen = false;
       this.oldFocus.focus();
       sendGAEvent({
-        eventCategory: "Sticky Banner",
-        eventAction: "Dismiss sticky banner",
+        eventCategory: "news:page:featured",
+        eventAction: "modal:sticky",
+        eventLabel: "sticky:dimiss",
       });
     },
   };
