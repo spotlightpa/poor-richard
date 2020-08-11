f36d10729f6f27bf4c8269181dca58c2e111649b
Carl Johnson <me@carlmjohnson.net>
2020-08-11 10:01:11 -0400

    Add COVID alerts unsubscription management (#133)
    * Add COVID alerts unsubscription management
    
    * Refinements
    
    * Fix format

--- src/esbuild/enhancements.js
+++ src/esbuild/enhancements.js
@@ -1,16 +1,18 @@
 import "alpinejs";
 
+import emailPreferences from "../enhancements/email-preferences.js";
 import embedList from "../enhancements/embed-list.js";
 import feed from "../enhancements/feed.js";
 import intersector from "../enhancements/intersector.js";
 import modal from "../enhancements/modal.js";
 import mostPopular from "../enhancements/most-popular.js";
 import readmore from "../enhancements/read-more.js";
-import sticky from "../enhancements/sticky.js";
 import searchArticles from "../enhancements/search-articles.js";
 import searchModal from "../enhancements/search-modal.js";
+import sticky from "../enhancements/sticky.js";
 
 window.spl = Object.assign({}, window.spl, {
+  emailPreferences,
   embedList,
   feed,
   intersector,
