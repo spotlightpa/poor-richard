d127330ac7118a5ed44d76f967c4f3e38d4519ac
Carl Johnson <me@carlmjohnson.net>
2020-08-21 14:38:08 -0400

    WIP Rewrite featured template in Tailwind.css

--- postcss.config.js
+++ postcss.config.js
@@ -1,24 +1,18 @@
 const purgecss = require("@fullhuman/postcss-purgecss")({
   content: ["./hugo_stats.json"],
-  extractors: [
-    {
-      extractor: (content) => {
-        let els = JSON.parse(content).htmlElements;
-        return els.tags.concat(els.classes, els.ids);
-      },
-      extensions: ["json"],
-    },
-  ],
+  defaultExtractor: (content) => {
+    let els = JSON.parse(content).htmlElements;
+    return els.tags.concat(els.classes, els.ids);
+  },
   whitelistPatternsChildren: [
     /align|disabled|multiple|readonly|rows|type=checkbox|type=radio|type|x-cloak/,
   ],
 });
 
-let plugins = [require("autoprefixer")];
-if (process.env.HUGO_ENVIRONMENT === "production") {
-  plugins.push(purgecss);
-}
-
 module.exports = {
-  plugins,
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.HUGO_ENVIRONMENT === "production" ? [purgecss] : []),
+  ],
 };
---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      serif: "Merriweather, serif",
+      sans:
+        'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", ' +
+        'Roboto, "Helvetica Neue", Arial, "Noto Sans", "Apple Color Emoji", ' +
+        '"Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji", sans-serif',
+    },
+    fontSize: {
+      xs: "0.7rem",
+      sm: "0.8rem",
+      base: "1rem",
+      lg: "1.3rem",
+      xl: "1.4rem",
+      "2xl": "1.5rem",
+      "3xl": "2rem",
+      "4xl": "2.4rem",
+      "5xl": "3rem",
+      "6xl": "3.4em",
+    },
+    extend: {
+      lineHeight: {
+        hed: "1.15",
+      },
+      colors: {
+        yellow: "#ffcb05",
+        darkblue: "#22416e",
+      },
+      spacing: {
+        "16x9": `${(100 * 9) / 16}%`,
+        "6x4": `${(100 * 4) / 6}%`,
+      },
+      screens: {
+        md: "850px",
+      },
+      maxWidth: {
+        content: "730px",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+};
