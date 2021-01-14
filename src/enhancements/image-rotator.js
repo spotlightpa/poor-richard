7905fb8bfd2a100cce834a9befee00478f86124b
Carl Johnson <me@carlmjohnson.net>
2021-01-14 13:54:36 -0500

    Promo: Use floats; trim spaces

--- src/enhancements/image-rotator.js
+++ src/enhancements/image-rotator.js
@@ -1,7 +1,7 @@
 import randomChoice from "../utils/random-choice.js";
 
 export default function imageRotator(rawChoices) {
-  let choices = rawChoices.split(",");
+  let choices = rawChoices.split(",").map(s=>s.trim());
   return {
     choice: randomChoice(choices),
   };
