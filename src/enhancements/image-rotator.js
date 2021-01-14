f8a3657db51bd3bfd7565f4b3840ba207c52d61b
Carl Johnson <me@carlmjohnson.net>
2021-01-14 14:41:04 -0500

    JS: Format

--- src/enhancements/image-rotator.js
+++ src/enhancements/image-rotator.js
@@ -1,7 +1,7 @@
 import randomChoice from "../utils/random-choice.js";
 
 export default function imageRotator(rawChoices) {
-  let choices = rawChoices.split(",").map(s=>s.trim());
+  let choices = rawChoices.split(",").map((s) => s.trim());
   return {
     choice: randomChoice(choices),
   };
