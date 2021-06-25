be740852a837c1db5755cd9d25dc7c7d88f49547
Carl Johnson <me@carlmjohnson.net>
2021-06-25 10:26:53 -0400

    Social buttons: more consistent show/hide; fix missing target bug

--- src/utils/social.js
+++ src/utils/social.js
@@ -1,4 +1,4 @@
-import { each, on } from "./dom-utils.js";
+import { on } from "./dom-utils.js";
 import { buildAndSend } from "./google-analytics.js";
 
 export function addSocialButtonListeners() {
@@ -29,28 +29,24 @@ export function addSocialButtonListeners() {
   });
 
   on("click", '[data-share="sharesheet"]', (ev) => {
+    let { currentTarget } = ev;
     let title =
-      ev.currentTarget.dataset.shareTitle ??
+      currentTarget.dataset.shareTitle ??
       document.querySelector("[itemprop='name']")?.content ??
       document.title;
     let text =
-      ev.currentTarget.dataset.shareText ??
+      currentTarget.dataset.shareText ??
       document.querySelector("[itemprop='description']")?.content ??
       "";
-    let url = ev.currentTarget.dataset.shareUrl ?? window.location.href;
-
+    let url = currentTarget.dataset.shareUrl ?? window.location.href;
     navigator
       .share({
         title,
         text,
         url,
       })
-      .then(() => buildAndSend(ev.currentTarget))
+      .then(() => buildAndSend(currentTarget))
       // Ignore errors caused by closing sheet
       .catch(() => null);
   });
-
-  if ("share" in navigator) {
-    each('[data-share="sharesheet"]', (el) => el.classList.remove("is-hidden"));
-  }
 }
