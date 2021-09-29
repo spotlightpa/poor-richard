6d4f4fb211314a7209f15ab9a558ba06a1374f5c
Carl Johnson <me@carlmjohnson.net>
2021-09-29 15:46:33 -0400

    JS: Use x-intersect for lightbox

--- src/enhancements/lightbox.js
+++ src/enhancements/lightbox.js
@@ -3,55 +3,35 @@ import { buildAndSend } from "../utils/google-analytics.js";
 export default function lightbox() {
   return {
     state: "initial",
-    intersectThreshold: 0.75,
 
     init() {
-      if (!("IntersectionObserver" in window)) {
-        return;
-      }
-      let ioOpts = {
-        threshold: [0, 0.75, 1.0],
-      };
-      let o = new IntersectionObserver(([entry]) => {
-        this.callback(entry);
-      }, ioOpts);
-      o.observe(this.$el);
       this.$watch("state", (val) => {
         buildAndSend(this.$el, {
           eventAction: `lightbox:${val}`,
         });
-        if (val === "active") {
-          this.$dispatch("close-sticky");
-        }
       });
     },
 
-    callback(entry) {
-      let showing = entry.intersectionRatio > this.intersectThreshold;
-      switch (this.state) {
-        case "initial":
-        case "passed":
-          if (showing) {
-            this.state = "active";
-          }
-          break;
-        case "active":
-        case "dismissed":
-          if (!showing) {
-            this.state = "passed";
-          }
-          break;
+    enter() {
+      if (this.state === "initial" || this.state === "passed") {
+        this.state = "active";
       }
     },
 
-    get active() {
-      return this.state === "active";
+    leave() {
+      if (this.state === "active" || this.state === "dismissed") {
+        this.state = "passed";
+      }
     },
 
     dismiss() {
       if (this.active) {
         this.state = "dismissed";
       }
+    },
+
+    get active() {
+      return this.state === "active";
     },
   };
 }
