8c5ad9ac9926cc61d0a53fcaaf186ae1a966b121
Carl Johnson <me@carlmjohnson.net>
2021-10-29 11:36:10 -0400

    JS: Use cached shuffle for image rotate

--- src/enhancements/image-rotator.js
+++ src/enhancements/image-rotator.js
@@ -1,10 +1,25 @@
-import randomChoice from "../utils/random-choice.js";
+import shuffle from "../utils/random-shuffle.js";
+
+let choiceCache = new Map();
 
 export default function imageRotator(choices) {
+  if (!choices) {
+    choices = [];
+  }
+  choices.sort();
+  let key = JSON.stringify(choices);
+  let [shuffledChoices = null, i = 0] = choiceCache.get(key) ?? [];
+  if (!shuffledChoices) {
+    shuffledChoices = shuffle(choices);
+    choiceCache.set(key, [shuffledChoices, i]);
+  }
   return {
-    choice: "",
-    init() {
-      this.choice = randomChoice(choices);
+    get choice() {
+      let choice = shuffledChoices[i];
+      i++;
+      i %= shuffledChoices.length;
+      choiceCache.set(key, [shuffledChoices, i]);
+      return choice;
     },
   };
 }
