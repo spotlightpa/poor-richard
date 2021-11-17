e55ac0776048acf26e886d2db9c0fbacdaf4bd79
Carl Johnson <me@carlmjohnson.net>
2021-11-17 16:20:25 -0500

    JS: Use x-trap for search modal

--- src/enhancements/search-modal.js
+++ src/enhancements/search-modal.js
@@ -1,39 +1,17 @@
-import { focus, blur } from "../utils/active-element.js";
-import { buildAndSend } from "../utils/google-analytics.js";
-
 export default function searchModal() {
-  const transitionLength = 500;
-
   return {
     isOpen: false,
 
+    init() {
+      this.$watch("isOpen", (val) => {
+        if (!val) {
+          this.$report({ target: this.$refs.closer });
+        }
+      });
+    },
+
     get modalClass() {
       return { "is-active": this.isOpen };
-    },
-
-    open() {
-      this.isOpen = true;
-
-      document.body.parentElement.classList.add("is-clipped");
-      window.setTimeout(
-        () => focus(this.$el.querySelector("input")),
-        transitionLength
-      );
-    },
-
-    close(sendEvent = true) {
-      this.isOpen = false;
-      document.body.parentElement.classList.remove("is-clipped");
-      window.setTimeout(
-        () => blur(this.$el.querySelector("input")),
-        transitionLength
-      );
-
-      if (sendEvent) {
-        buildAndSend(this.$el, {
-          eventAction: "modal:search:dismiss",
-        });
-      }
     },
   };
 }
