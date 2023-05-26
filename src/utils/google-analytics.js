27aaeb544adc0f7e4cb4e83d2efd3a750fba9ccf
Carl Johnson <me@carlmjohnson.net>
2023-05-26 12:10:12 -0400

    google-analytics: work in progress Plausible mode

--- src/utils/google-analytics.js
+++ src/utils/google-analytics.js
@@ -11,15 +11,90 @@ export const DO_NOT_TRACK_KEY = "do-not-track";
 
 let dnt = loadItem(DO_NOT_TRACK_KEY);
 
+function buildPlausible(...args) {
+  let [send, action, params] = args;
+  if (send !== "send") return ["", null];
+  if (action === "pageview") {
+    let kind = allClosest(document.body, "[data-ga-label]")
+      .map((el) => el.dataset.gaLabel)
+      .join(":");
+    let el = document.querySelector("[data-ga-settings]");
+    let title = document.title;
+    if (el) {
+      title = el.dataset.gaPageTitle;
+    } else {
+      // eslint-disable-next-line no-console
+      console.warn("could not find page title!");
+    }
+    return ["pageview", { "page-kind": kind, title }];
+  }
+  if (action === "exception") {
+    return ["exception", params];
+  }
+  if (action !== "event") {
+    // eslint-disable-next-line no-console
+    console.warn("unknown action!", action);
+    return ["", null];
+  }
+  let { eventCategory, eventAction, eventLabel, nonInteraction } = params;
+  if (nonInteraction) {
+    return ["", null];
+  }
+  if (eventAction.startsWith("http")) {
+    return [
+      "link",
+      {
+        "page-kind": eventLabel,
+        component: eventCategory,
+        target: eventAction,
+      },
+    ];
+  }
+  if (eventAction.startsWith("view")) {
+    return [
+      "scrollto",
+      {
+        "page-kind": eventLabel,
+        component: eventCategory,
+      },
+    ];
+  }
+  return [
+    "event",
+    { action: eventAction, "page-kind": eventLabel, component: eventCategory },
+  ];
+}
+
+function sendPlausible(action, params = {}) {
+  if (!action) return;
+  let payload = {
+    n: action,
+    u: location.href,
+    d: document.domain,
+    r: document.referrer,
+    w: window.innerWidth,
+    h: 0, // "H"ashmode, not "h"eight
+    p: params,
+  };
+  navigator.sendBeacon(
+    "https://plausible.io/api/event",
+    JSON.stringify(payload)
+  );
+}
+
+// TODO: Remove callGA once we have enough data about switching to Plausible or not
 export function callGA(...args) {
+  let [action, params] = buildPlausible(...args);
   if (dnt) {
     /* eslint-disable no-console */
+    console.log("Plausible", action, params);
     console.group("Google Analytics Debug");
     for (let arg of args) console.log("%o", arg);
     console.groupEnd();
     /* eslint-enable no-console */
     return;
   }
+  sendPlausible(action, params);
   galite(...args);
 }
 
