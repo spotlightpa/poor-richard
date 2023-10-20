6a776d9b71ff21151fdb4d467ff68ae6fedb7208
Carl Johnson <me@carlmjohnson.net>
2023-10-20 15:40:19 -0400

    search-articles: Add ability to load next/previous page of results

--- src/enhancements/search-articles.js
+++ src/enhancements/search-articles.js
@@ -1,7 +1,21 @@
 import { apdate } from "journalize";
 
 import imgproxy from "../utils/imgproxy-url.js";
-import searchAPI from "../utils/search-api.js";
+import fetchJSON from "../utils/fetch-json.js";
+
+const appID = `5M1ASV9W0A`;
+const publicKey = `fd9492397caaffd9cb49be210170e63a`;
+const indexName = `spotlightpa-content`;
+
+let baseURL = `https://${appID}-dsn.algolia.net/1/indexes/${indexName}?x-algolia-agent=spotlightpa&x-algolia-application-id=${appID}&x-algolia-api-key=${publicKey}&`;
+
+function searchAPI(query, page = 0) {
+  if (!query) {
+    return Promise.resolve(null);
+  }
+  let params = { query, page };
+  return fetchJSON(baseURL + new URLSearchParams(params).toString());
+}
 
 function roundUp(n, by) {
   return by * Math.ceil(n / by);
@@ -43,10 +57,17 @@ export default function searchArticles() {
     results: null,
     error: null,
     isLoading: false,
+    page: 0,
     magicPixel,
 
     init() {
-      this.$watch("query", () => this.search());
+      this.$watch("query", () => {
+        this.page = 0;
+        this.search();
+      });
+      this.$watch("page", () => {
+        if (this.page) this.search();
+      });
     },
 
     search() {
@@ -57,7 +78,7 @@ export default function searchArticles() {
         return;
       }
       this.isLoading = true;
-      searchAPI(this.query)
+      searchAPI(this.query, this.page)
         .then((results) => {
           this.error = null;
           if (results) {
@@ -94,9 +115,12 @@ export default function searchArticles() {
       if (nHits === 1) {
         return "Got one search result.";
       }
-      let nStories = this.results?.hits?.length ?? 0;
-      let more = nHits > nStories ? `Showing first ${nStories}.` : "";
-      return `Got ${nHits} search results. ${more}`;
+      let msg = `Got ${nHits} search results.`;
+      let nbPages = this.results?.nbPages;
+      if (nbPages > 1) {
+        msg += ` Showing results page ${this.page + 1} of ${nbPages}.`;
+      }
+      return msg;
     },
 
     loadImage(ev) {
@@ -110,6 +134,36 @@ export default function searchArticles() {
       width = roundUp(window.devicePixelRatio * width, 1);
       height = Math.round(aspectRatio * width);
       el.src = imgproxy(src, { width, height });
+    },
+
+    get hasNextPage() {
+      let nbPages = this.results?.nbPages ?? 0;
+      return this.page + 1 < nbPages;
+    },
+
+    async nextPage() {
+      this.page++;
+      // Seems to need to wait to scroll to prevent layout jank
+      window.setTimeout(
+        () =>
+          this.$refs.top.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+          }),
+        150,
+      );
+    },
+
+    get hasPreviousPage() {
+      return this.page - 1 >= 0;
+    },
+
+    previousPage() {
+      this.page--;
+      this.$refs.top.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
     },
   };
 }
