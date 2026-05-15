b331d83b8e21f41305e5301a45844a68f7ff46cc
Carlana <mail@carlana.net>
2026-05-15 15:46:37 -0400

    Remove tailwind.config.js, run prettier
    We didn't fully move to a Tailwind v4 CSS config before, and this was confusing our prettier tailwind plugin.

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,5 @@
 export default {
-  tailwindConfig: "tailwind.config.js",
+  tailwindStylesheet: "assets/css/tw.css",
   plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-go-template"],
   overrides: [
     {
